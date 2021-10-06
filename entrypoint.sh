7309b112f5be371b18fb787783a42fa94f00ee87
yzernik <yzernik@gmail.com>
2021-10-05 23:30:35 -0700

    Try create and push tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,11 @@ echo "$(cat /ots-git-gpg-wrapper.sh)"
 
 echo "$(git status)"
 echo "$(git log)"
+
+$TAG_NAME = $(openssl rand -base64 12)
+
+git tag $TAG_NAME
+
+echo "$(git tag)"
+
+git push origin $TAG_NAME
