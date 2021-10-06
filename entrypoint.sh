e9b35b8e0644a81b21aa7ded1b2f871074e505c7
yzernik <yzernik@gmail.com>
2021-10-06 03:28:29 -0700

    Organize script

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@
 echo "hello $1"
 echo "repo name: $2"
 
-# # Generate GPG key
+# Generate GPG key
 echo "$(gpg --batch --passphrase '' --quick-generate-key "ots-action-key" rsa4096)"
 KEY_ID="$(gpg --list-secret-keys --keyid-format=long --with-colons | awk -F: '/sec:u:4096:1:/ {print $5}')"
-echo "Using key id: $(echo $KEY_ID)"
 
+# Configure git
 git config --global user.email "fake@email.com"
 git config --global user.name "OpenTimestamps Github Action"
 git config --global user.signingkey $KEY_ID
@@ -16,9 +16,7 @@ git config --global gpg.program /ots-git-gpg-wrapper.sh
 TAG_NAME=$(openssl rand -hex 12)
 TAG_MESSAGE="This commit belongs to repository: $2."
 
-#git tag $TAG_NAME
+# Create and push signed tag
 git tag -s -m "$TAG_MESSAGE" $TAG_NAME HEAD
-
 echo "$(git tag)"
-
 git push origin $TAG_NAME
