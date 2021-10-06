78930da1ea35e479b72080d2f2115df376189595
yzernik <yzernik@gmail.com>
2021-10-06 03:01:28 -0700

    Use inputs in action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,16 @@
 #!/bin/sh -l
 
 echo "hello $1"
+echo "repo name: $2"
 
 # # Generate GPG key
-echo "$(gpg --batch --passphrase '' --quick-generate-key "example-14" rsa4096)"
+echo "$(gpg --batch --passphrase '' --quick-generate-key "ots-action-key" rsa4096)"
 KEY_ID="$(gpg --list-secret-keys --keyid-format=long --with-colons | awk -F: '/sec:u:4096:1:/ {print $5}')"
 echo "Using key id: $(echo $KEY_ID)"
 
 git config --global user.email "fake@email.com"
-git config --global user.name "Fake name"
+git config --global user.name "Fake Name"
 git config --global user.signingkey $KEY_ID
-git config --global gpg.program gpg
-#GIT_TRACE=1 git tag -s -m 'Hello World!' test-tag HEAD
 git config --global gpg.program /ots-git-gpg-wrapper.sh
 
 TAG_NAME=$(openssl rand -hex 12)
