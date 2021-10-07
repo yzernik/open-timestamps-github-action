1b948a34e4f3a9854081e100033145d3edc0cfba
yzernik <yzernik@gmail.com>
2021-10-06 22:12:38 -0700

    Set branch name and wait option

--- ots-git-gpg-wrapper.sh
+++ ots-git-gpg-wrapper.sh
@@ -5,5 +5,5 @@
 # Required because git's gpg.program option doesn't allow you to set command
 # line options; see the doc/git-integration.md
 
-ots-git-gpg-wrapper --wait --gpg-program "`which gpg`" -- "$@"
-# ots-git-gpg-wrapper --gpg-program "`which gpg`" -- "$@"
+# ots-git-gpg-wrapper --wait --gpg-program "`which gpg`" -- "$@"
+ots-git-gpg-wrapper --gpg-program "`which gpg`" -- "$@"
