6a3c6d4c8df20ee59e92e6b07b8051376e838775
yzernik <yzernik@gmail.com>
2021-10-06 20:06:02 -0700

    Try using += for append tag message

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,8 +32,8 @@ git config --global gpg.program /ots-git-gpg-wrapper.sh
 
 NEW_TAG_NAME="$TAG_NAME-ots"
 TAG_MESSAGE="$(printf "OpenTimestamps Github Action")"
-TAG_MESSAGE="$(printf "${TAG_MESSAGE}\nGithub repository: $GITHUB_REPOSITORY")"
-TAG_MESSAGE="$(printf "${TAG_MESSAGE}\nGithub actor: $GITHUB_ACTOR")"
+TAG_MESSAGE+="$(printf "\nGithub repository: $GITHUB_REPOSITORY")"
+TAG_MESSAGE+="$(printf "\nGithub actor: $GITHUB_ACTOR")"
 
 # Append custom message if exists
 if [[ -z "$CUSTOM_MESSAGE" ]]
